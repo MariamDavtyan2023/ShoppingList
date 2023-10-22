a943a14d7df403d91de53dd80669f29b5c8833bc
MariamDavtyan2023 <davtyan_mariam91@mail.ru>
2023-10-22 16:56:29 +0400

    Setting a fragment

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -8,53 +8,46 @@ import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
 
-    companion object {
-        const val ITEM_TYPE_ENABLED = 1
-        const val ITEM_TYPE_DISABLED = 2
-
-        const val MAX_POOL_SIZE = 15
-    }
-
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
-    var onShopItemShortClickListener: ((ShopItem) -> Unit)? = null
-
+    var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val layout = if (viewType == ITEM_TYPE_ENABLED) {
-            R.layout.item_shop_enabled
-        } else {
-            R.layout.item_shop_disabled
+        val layout = when (viewType) {
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            else -> throw RuntimeException("Unknown view type: $viewType")
         }
-        val view = LayoutInflater.from(parent.context).inflate(
-            layout,
-            parent,
-            false
-        )
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ShopItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         val shopItem = getItem(position)
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
-        holder.view.setOnLongClickListener {
+        viewHolder.view.setOnLongClickListener {
             onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-        holder.view.setOnClickListener {
-            onShopItemShortClickListener?.invoke(shopItem)
+        viewHolder.view.setOnClickListener {
+            onShopItemClickListener?.invoke(shopItem)
         }
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
     }
-
 
     override fun getItemViewType(position: Int): Int {
-        val shopItem = getItem(position)
-        return if (shopItem.enabled) {
-            ITEM_TYPE_ENABLED
+        val item = getItem(position)
+        return if (item.enabled) {
+            VIEW_TYPE_ENABLED
         } else {
-            ITEM_TYPE_DISABLED
+            VIEW_TYPE_DISABLED
         }
     }
 
+    companion object {
 
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+
+        const val MAX_POOL_SIZE = 30
+    }
 }
